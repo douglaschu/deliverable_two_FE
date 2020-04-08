c15c9b6dff77bc0a6393399cfafd3565ac4c4fef
douglaschu <douglas.l.chu@gmail.com>
2020-04-08 17:16:58 -0400

    Corrected version based on grading feedback

--- js/script.js
+++ js/script.js
@@ -1,57 +1,22 @@
 //Declaring variables
 let vacationType = prompt("Hello, and welcome to TRAVEL-MATIC 3000.\n\nChoosing the vacation of your dreams has never been easier with TRAVEL-MATIC 3000!\n\nWhat kind of trip would you like to go on, musical, tropical, or adventurous?");
 let groupSize = prompt("With our state-of-the-art PARTY-VISION technology, TRAVEL-MATIC 3000 can now suggest the perfect way to travel to your dream getaway.\n\nHow many people, including yourself, will be in your group?");
-let result; //Declaring result as an empty variable, as its value is dependent on how the user prompts affect the conditionals
- 
-if (vacationType === "musical" && groupSize >= 0) {
-    result = `For the ${vacationType} experience of a lifetime, you should go to New Orleans and you should fly first class.`;
-} else if (vacationType === "musical" && groupSize >= 3 && groupSize <= 5) {
-    result = `For the ${vacationType} experience of a lifetime, you should go to New Orleans and you should go there by helicopter.`;
-} else if (vacationType === "musical" && groupSize >= 6) {
-    result = `For the ${vacationType} experience of a lifetime, you should go to New Orleans and you should charter a flight to get there.`
-}
+let destination = (vacationType === "musical") ? "New Orleans"
+                : (vacationType === "tropical") ? "a beach vacation in Mexico"
+                : (vacationType === "adventurous") ? "go whitewater rafting down the Grand Canyon"
+                : false;
+let travelMode = (groupSize > 0 && groupSize <= 2) ? "first class flight"
+               : (groupSize >= 3 && groupSize <= 5) ? "helicopter ride"
+               : (groupSize >= 6) ? "chartered private plane"
+               : false;
+let result = `Since you are a group of ${groupSize} going on a ${vacationType} vacation, you should take a ${travelMode} to ${destination}.`;
 
-if (vacationType === "tropical" && groupSize >= 0) {
-    result = `For a ${vacationType} getaway, you should take a beach vacation to Mexico and you should fly first class.`;
-} else if (vacationType === "tropical" && groupSize >= 3 && groupSize <=5) {
-    result = `For a ${vacationType} getaway, you should take a beach vacation to Mexico and you should go there by helicopter.`;
-} else if (vacationType === "tropical" && groupSize >= 6) {
-    result = `For a ${vacationType} getaway, you should take a beach vacation to Mexico and you should charter a flight to get there.`;
-}
-
-if (vacationType === "adventurous" && groupSize >= 0) {
-    result = `If you're feeling ${vacationType}, you should go whitewater rafting down the Grand Canyon and you should fly first class to get there.`;
-} else if (vacationType === "adventurous" && groupSize >= 3 && groupSize <= 5) {
-    result = `If you're feeling ${vacationType}, you should go whitewater rafting down the Grand Canyon and you should go there by helicopter.`;
-} else if (vacationType === "adventurous" && groupSize >= 6) {
-    result = `If you're feeling ${vacationType}, you should go whitewater rafting down the Grand Canyon and you should charter a flight to get there.`;
-}
-
-//having some fun here with non-standard user inputs to the prompt
-if (vacationType !== "musical" && vacationType !== "tropical" && vacationType !== "adventurous") {
-    result = "You did not choose a musical, tropical, or adventurous vacation.\n\nTRAVEL-MATIC 3000 has determined these are the three most optimal vacation types for humans.\n\n\Please restart TRAVEL-MATIC 3000 and try again.";
-}
+//having some fun here with "wrong" user inputs to the prompt
+if (destination === false) {
+    result = "You did not choose a musical, tropical, or adventurous vacation.\n\nTRAVEL-MATIC 3000 has determined these are the three most optimal vacation types for humans.\n\n\Please restart TRAVEL-MATIC 3000 and try again to receive your ideal vacation recommendation.";
+} else if (travelMode === false) {
+    result = "Please do not try to goof TRAVEL-MATIC 3000 by entering physically impossible numbers for your group size.\n\n\TRAVEL-MATIC 3000 takes its responsibilities very seriously.\n\nPlease restart TRAVEL-MATIC 3000 and try again to receive your ideal vacation recommendation.";
+} 
 
 //The lovely output message
 console.log(`${result} \n\nThank you for choosing TRAVEL-MATIC 3000. \n\nThe vacation of your dreams awaits!`);
-
-
-
-//First attempt, works but doesn't use result variable
-// if (vacationType === "musical") {
-//     console.log(`For the ${vacationType} experience of a lifetime, you should go to New Orleans `);
-// } else if (vacationType === "tropical") {
-//     console.log(`For a ${vacationType} getaway, you should take a beach vacation to Mexico `);
-// } else if (vacationType === "adventurous") {
-//     console.log(`If you're feeling ${vacationType}, you should go whitewater rafting down the Grand Canyon `);
-// }
-
-// if (groupSize >= 0) {
-//     console.log(`and you should fly first class.`);
-// } else if (groupSize >=3 && groupSize <=5) {
-//     console.log(`and you should take a helicopter to get there. `);
-// } else if (groupSize >= 6) {
-//     console.log("and you should charter a flight to get there.");
-// }
-
-// console.log(`Thank you!`);
